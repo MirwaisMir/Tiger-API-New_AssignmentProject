fd55762440413e9aad55b62afedefd9d72ba89a5
Mirwais Mir <Mirwais.mirzakwal1@gmail.com>
2022-07-27 22:33:38 -0400

    i wrote some comments in runner class for testing status

--- src/test/java/Tigers/TestRunner.java
+++ src/test/java/Tigers/TestRunner.java
@@ -2,7 +2,7 @@ package Tigers;
 import com.intuit.karate.junit5.Karate;
 public class TestRunner {
 	@Karate.Test
-	// lets make some changes 
+	// lets make some changes // this is the new changes i made 
   public  Karate  runTest() {
         return Karate.run("classpath:features")
                 .tags("@smoke");
