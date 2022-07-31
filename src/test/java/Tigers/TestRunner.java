e7fff8ba9d5ffd53a59bc654dcad90b9b5294122
Mirwais Mir <Mirwais.mirzakwal1@gmail.com>
2022-07-31 19:23:54 -0400

    Today sunday i wrote some comment in runner class for paractice purpose

--- src/test/java/Tigers/TestRunner.java
+++ src/test/java/Tigers/TestRunner.java
@@ -3,6 +3,7 @@ import com.intuit.karate.junit5.Karate;
 public class TestRunner {
 	@Karate.Test
 	// lets make some changes // this is the new changes i made 
+	// I want to put some comments for paractice purposes
   public  Karate  runTest() {
         return Karate.run("classpath:features")
                 .tags("@smoke");
